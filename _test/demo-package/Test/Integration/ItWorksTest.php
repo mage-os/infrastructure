0cc7cc963b0b48e8effcd7f973c33044fa500255
Damien Retzinger <damienwebdev@gmail.com>
2022-08-12 10:25:11 -0400

    ci: add runnable integration tests from infra

---
+++ _test/demo-package/Test/Integration/ItWorksTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MageOS\DemoPackage\Test\Integration;
+
+class ItWorksTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testItWorks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
